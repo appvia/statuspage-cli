e17c6f2a0ec09029a81ef190de2339fa305dd0fe
Mohammud Yassine Jaffoo <mohammud.jaffoo@digital.homeoffice.gov.uk>
2020-08-05 10:42:03 +0100

    add updateComponent and allow component to be showcased

--- cmd/component.go
+++ cmd/component.go
@@ -30,6 +30,7 @@ var componentID string
 var componentDescription string
 var componentStatus string
 var componentName string
+var componentShowcase bool
 var url string
 
 var getComponentCmd = &cobra.Command{
@@ -83,8 +84,49 @@ var createComponentCmd = &cobra.Command{
 			os.Exit(1)
 		}
 
-		var jsonStr = []byte(fmt.Sprintf(`{"component": {"description": "%s", "status": "%s", "name": "%s"}}`, componentDescription, componentStatus, componentName))
+		var jsonStr = []byte(fmt.Sprintf(`{"component": {"name": "%s", "description": "%s", "status": "%s", "showcase": %t}}`, componentName, componentDescription, componentStatus, componentShowcase))
 		request, err := http.NewRequest("POST", "https://api.statuspage.io/v1/pages/"+pageID+"/components", bytes.NewBuffer(jsonStr))
+		request.Header.Set("Authorization", "OAuth "+apiKey)
+		request.Header.Set("Content-Type", "application/json")
+
+		client := &http.Client{Timeout: time.Second * 10}
+		resp, err := client.Do(request)
+
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		defer resp.Body.Close()
+
+		body, err := ioutil.ReadAll(resp.Body)
+
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		fmt.Println(string(body))
+	},
+}
+
+var updateComponentCmd = &cobra.Command{
+	Use:   "component",
+	Short: "Update a component.",
+	Run: func(cmd *cobra.Command, args []string) {
+		apiKeyFromEnv := utils.GetEnv("API_KEY")
+		apiKey = utils.DefaultToEnv(apiKeyFromEnv, apiKey)
+
+		allowedComponentStatuses := []string{"operational", "under_maintenance", "degraded_performance", "partial_outage", "major_outage"}
+
+		if (utils.Contains(allowedComponentStatuses, componentStatus)) != true {
+			cmd.Help()
+			os.Exit(1)
+		}
+
+		var jsonStr = []byte(fmt.Sprintf(`{"component": {"description": "%s", "status": "%s", "showcase": %t}}`, componentDescription, componentStatus, componentShowcase))
+		// fmt.Println(string(jsonStr))
+		request, err := http.NewRequest("PATCH", "https://api.statuspage.io/v1/pages/"+pageID+"/components/"+componentID, bytes.NewBuffer(jsonStr))
 		request.Header.Set("Authorization", "OAuth "+apiKey)
 		request.Header.Set("Content-Type", "application/json")
 
@@ -136,7 +178,7 @@ var deleteComponentCmd = &cobra.Command{
 			os.Exit(1)
 		}
 
-		fmt.Println(string(body))
+		fmt.Println(resp.StatusCode, string(body))
 	},
 }
 
@@ -147,19 +189,30 @@ func init() {
 	getComponentCmd.MarkFlagRequired("page-id")
 	createComponentCmd.Flags().StringVarP(&apiKey, "api-key", "k", "", "API_KEY environment variable. API key to authenticate against the status page API (required)")
 	createComponentCmd.Flags().StringVarP(&pageID, "page-id", "p", "", "Page identifier (required)")
-	createComponentCmd.Flags().StringVarP(&componentName, "name", "n", "", "Display name for component")
-	createComponentCmd.Flags().StringVarP(&componentDescription, "description", "d", "", "More detailed description for component")
-	createComponentCmd.Flags().StringVarP(&componentStatus, "status", "s", "", "Status of the component. Valid choices are: operational, under_maintenance, degraded_performance, partial_outage, major_outage.")
+	createComponentCmd.Flags().StringVarP(&componentName, "name", "n", "", "Display name for component (required)")
+	createComponentCmd.Flags().StringVarP(&componentDescription, "description", "d", "", "More detailed description for component (required)")
+	createComponentCmd.Flags().StringVarP(&componentStatus, "status", "s", "", "Status of the component. Valid choices are: operational, under_maintenance, degraded_performance, partial_outage, major_outage (required)")
+	createComponentCmd.Flags().BoolVarP(&componentShowcase, "showcase", "c", false, "Should this component be showcased")
 	createComponentCmd.MarkFlagRequired("page-id")
 	createComponentCmd.MarkFlagRequired("name")
 	createComponentCmd.MarkFlagRequired("description")
 	createComponentCmd.MarkFlagRequired("status")
+	updateComponentCmd.Flags().StringVarP(&apiKey, "api-key", "k", "", "API_KEY environment variable. API key to authenticate against the status page API (required)")
+	updateComponentCmd.Flags().StringVarP(&pageID, "page-id", "p", "", "Page identifier (required)")
+	updateComponentCmd.Flags().StringVarP(&componentID, "id", "i", "", "Component identifier (required)")
+	updateComponentCmd.Flags().StringVarP(&componentDescription, "description", "d", "", "More detailed description for component")
+	updateComponentCmd.Flags().StringVarP(&componentStatus, "status", "s", "", "Status of the component. Valid choices are: operational, under_maintenance, degraded_performance, partial_outage, major_outage (required)")
+	updateComponentCmd.Flags().BoolVarP(&componentShowcase, "showcase", "c", false, "Should this component be showcased")
+	updateComponentCmd.MarkFlagRequired("page-id")
+	updateComponentCmd.MarkFlagRequired("id")
+	updateComponentCmd.MarkFlagRequired("status")
 	deleteComponentCmd.Flags().StringVarP(&apiKey, "api-key", "k", "", "API_KEY environment variable. API key to authenticate against the status page API (required)")
 	deleteComponentCmd.Flags().StringVarP(&componentID, "id", "i", "", "Component identifier (required)")
 	deleteComponentCmd.Flags().StringVarP(&pageID, "page-id", "p", "", "Page identifier (required)")
 	deleteComponentCmd.MarkFlagRequired("id")
 	deleteComponentCmd.MarkFlagRequired("page-id")
 	createCmd.AddCommand(createComponentCmd)
+	updateCmd.AddCommand(updateComponentCmd)
 	deleteCmd.AddCommand(deleteComponentCmd)
 	getCmd.AddCommand(getComponentCmd)
 }
