59daa66293fdb02195b22cbbaf56469b42d3ef81
Mohammud Yassine Jaffoo <mohammud.jaffoo@digital.homeoffice.gov.uk>
2020-08-07 11:12:47 +0100

    copyright to Appvia

--- cmd/update.go
+++ cmd/update.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2020 Mohammud Yassine Jaffoo <mohammudyassine.jaffoo@appvia.io>
+Copyright © 2020 Appvia Ltd <info@appvia.io>
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
